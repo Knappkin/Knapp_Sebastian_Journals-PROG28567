c5844971c0e9255dbb8c6d4e90fd4fed83d57d24
Knappkin <44062671+Knappkin@users.noreply.github.com>
2025-09-03 13:51:46 -0400

    Finished Journal 1 Square Spawner Task
    Implemented the first task of Journal 1, creating a square out of debug.drawlines. There is an outline so the player sees where they will be placed, which follows the mouse. The player can grow/shrink the square using the mouse wheel

---
+++ Assets/Scripts/Journal 1/SquareSpawner.cs
@@ -0,0 +1,85 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SquareSpawner : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private float squareSize;
+    private float scaleSize;
+
+    //Corners
+    Vector2 topLeft;
+    Vector2 topRight;
+    Vector2 bottomRight;
+    Vector2 bottomLeft;
+    void Start()
+    {
+        squareSize = 2f;
+        scaleSize = 1.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindCorners();
+
+        ShowOutline();
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            DrawSquare();
+        }
+
+        // scaleSize += Mathf.Clamp(Input.mouseScrollDelta.y * 0.1f, -0.5f, 1.5f);
+
+        scaleSize += Input.mouseScrollDelta.y * 0.1f;
+        scaleSize = Mathf.Clamp(scaleSize, 0.2f, 3f);
+    }
+
+    private void FindCorners()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+        float halfSize = squareSize * scaleSize /2;
+
+        //Top left (-x,+y)
+        topLeft = new Vector2(mousePos.x - halfSize, mousePos.y + halfSize);
+        Debug.DrawLine(mousePos, topLeft, Color.red);
+        //Top right (+x,+y)
+        topRight = new Vector2(mousePos.x + halfSize, mousePos.y + halfSize);
+        Debug.DrawLine(mousePos, topRight, Color.blue);
+
+        //Bottom right (+x,-y)
+        bottomRight = new Vector2(mousePos.x + halfSize, mousePos.y - halfSize);
+        Debug.DrawLine(mousePos, bottomRight, Color.yellow);
+        //Bottom left (-x, -y)
+        bottomLeft = new Vector2(mousePos.x - halfSize, mousePos.y - halfSize);
+        Debug.DrawLine(mousePos, bottomLeft, Color.green);
+    }
+
+    private void ShowOutline()
+    {
+        //Top side
+        Debug.DrawLine(topLeft, topRight, Color.cyan);
+
+        //Right side
+        Debug.DrawLine(topRight, bottomRight, Color.cyan);
+        //Bottom side
+        Debug.DrawLine(bottomLeft, bottomRight, Color.cyan);
+        //Left side
+        Debug.DrawLine(topLeft, bottomLeft, Color.cyan);
+    }
+    private void DrawSquare()
+    {
+        //Top Side
+        Debug.DrawLine(topLeft, topRight, Color.white);
+        //Right Side
+        Debug.DrawLine(topRight, bottomRight, Color.white);
+        //Bottom Side
+        Debug.DrawLine(bottomLeft, bottomRight, Color.white);
+        //Left Side
+        Debug.DrawLine(topLeft, bottomLeft, Color.white);
+    }
+
+}

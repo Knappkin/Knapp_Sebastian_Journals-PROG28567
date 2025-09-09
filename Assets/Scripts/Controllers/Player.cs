4ef1978916f7595a1f02f7ef4755d0bc928f28e0
Knappkin <44062671+Knappkin@users.noreply.github.com>
2025-09-09 11:53:17 -0400

    Added Ship Files
    Added the ship files to the project, completed the spawn bomb exercise

---
+++ Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public List<Transform> asteroidTransforms;
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public Transform bombsTransform;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SpawnBombAtOffset(new Vector3(0, 1));
+        }
+    }
+
+   public void SpawnBombAtOffset(Vector3 inOffset)
+    {
+        StartCoroutine(PlaceBomb(inOffset));
+    }
+
+    public IEnumerator PlaceBomb(Vector3 inOffset)
+    {
+        float t = Time.deltaTime;
+        while(t < 3)
+        {
+            t += Time.deltaTime;
+            yield return null;
+        }
+        GameObject bombPlaced = Instantiate(bombPrefab, transform.position + inOffset, Quaternion.identity);
+        Destroy(bombPlaced, 3);
+    }
+}

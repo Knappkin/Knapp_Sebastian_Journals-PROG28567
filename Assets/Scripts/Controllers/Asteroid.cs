625dd38d29e110eb97c7a8565b15a7eb9a3d96ed
Knappkin <44062671+Knappkin@users.noreply.github.com>
2025-09-21 23:11:16 -0400

    Tasks 2 & 3
    Forgot to commit task 2 before starting 3. Both are now complete. For task 2 I got the enemy to spawn on a random edge and accelerate in the direction of the player (the player location when they spawned). Task 3 got a random direction and had it move in that direction until it reaches a max distance, and then selects a new direction.

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -8,13 +8,41 @@ public class Asteroid : MonoBehaviour
     public float arrivalDistance;
     public float maxFloatDistance;
 
+    private Vector2 nextPosition;
+    private Vector2 moveDirection;
+
     // Start is called before the first frame update
     void Start()
     {
+        ChooseNextPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MoveAsteroid();
+        //Debug.DrawLine(transform.position, nextPosition);
+    }
+
+    private void ChooseNextPosition()
+    {
+        Vector2 randomDirection;
+        randomDirection.x = Random.Range(-1, 1);
+        randomDirection.y = Random.Range(-1, 1);
+
+        randomDirection = randomDirection.normalized;
+        moveDirection = randomDirection;
+
+        nextPosition = transform.position + (Vector3)randomDirection * maxFloatDistance;
+        
+    }
+
+    private void MoveAsteroid()
+    {
+        transform.position += (Vector3)moveDirection * moveSpeed * Time.deltaTime;
+        if((nextPosition-(Vector2)transform.position).magnitude < arrivalDistance)
+        {
+            ChooseNextPosition();
+        }
     }
 }
--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -3,8 +3,99 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
+
+    public GameObject playerShip;
+    private float baseSpeed;
+    private float maxSpeed;
+
+    private float accelTarget;
+    [SerializeField] private float accelMin;
+    [SerializeField] private float accelMax;
+
+    [SerializeField] private float accelTime;
+
+    [SerializeField] private float minBaseSpeed;
+    [SerializeField] private float maxBaseSpeed;
+
+    private Vector2 enemyVelo;
+
+    private float acceleration;
+
+    private Vector2 targetDirection;
+
+    private Vector2 screenPos;
+
+    private void Start()
+    {
+        SpawnOnEdge();
+    }
     private void Update()
     {
+
+        screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        MoveEnemy();
+        
+        if (screenPos.x < 0 || screenPos.x > Screen.width || screenPos.y < 0 || screenPos.y > Screen.height)
+        {
+            SpawnOnEdge();
+        }
+    }
+
+    private void SpawnOnEdge()
+    {
+        int spawnLocation = Random.Range(0, 4);
+        //int spawnLocation = 2;
+
+        float screenWidth = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width,0)).x;
+        float screenHeight = Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y;
+
+        float horizontalPos = Random.Range(-screenWidth, screenWidth);
+        float verticalPos = Random.Range(-screenHeight, screenHeight);
+        
+        //TOP EDGE
+        if (spawnLocation == 0)
+        {
+            Debug.Log("TOP");
+            transform.position = new Vector2(horizontalPos, screenHeight);
+        }
+        //RIGHT EDGE
+        if (spawnLocation == 1)
+        {
+            Debug.Log("RIGHT");
+            transform.position = new Vector2(screenWidth, verticalPos);
+        }
+        //BOTTOM EDGE
+        if (spawnLocation == 2)
+        {
+            Debug.Log("BOTTOM");
+            transform.position = new Vector2(horizontalPos, -screenHeight);
+        }
+        //LEFT EDGE
+        if (spawnLocation == 3)
+        {
+            Debug.Log("LEFT");
+            transform.position = new Vector2(-screenWidth, verticalPos);
+        }
+
+
+        //GET DIRECTION TO MOVE
+        targetDirection = (playerShip.transform.position - transform.position).normalized;
+
+        //SET ACCEL GOAL AND BASE SPEED
+        accelTarget = Random.Range(accelMin, accelMax);
+
+        acceleration = accelTarget / accelTime;
+
+        float baseSpeed = Random.Range(minBaseSpeed, maxBaseSpeed);
+
+        enemyVelo = targetDirection * baseSpeed;
+    }
+
+    private void MoveEnemy()
+    {
+        enemyVelo += targetDirection * acceleration * Time.deltaTime;
+
+        transform.position += (Vector3) enemyVelo * Time.deltaTime;
     }
 
 }
